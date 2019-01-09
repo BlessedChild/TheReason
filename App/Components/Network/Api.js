f0f4d2748d5028f19596e2d39879a9a195bf4068
Null <null@NulldeiMac.local>
2019-01-09 11:31:39 +0800

    ++

--- App/Components/Network/Api.js
+++ App/Components/Network/Api.js
@@ -9,7 +9,7 @@ export default class FetchExample extends React.Component {
   }
 
   componentDidMount(){
-    return fetch('https://facebook.github.io/react-native/movies.json')
+    return fetch('https://raw.githubusercontent.com/BlessedChild/TheReason/master/movies.json')
       .then((response) => response.json())
       .then((responseJson) => {
 
