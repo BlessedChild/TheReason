d39a2abc937251316c3c7f5b571311650b5243e1
Null <null@NulldeiMac.local>
2019-01-09 11:43:41 +0800

    ++

--- App/Components/Network/Api.js
+++ App/Components/Network/Api.js
@@ -9,7 +9,7 @@ export default class FetchExample extends React.Component {
   }
 
   componentDidMount(){
-    return fetch('https://raw.githubusercontent.com/BlessedChild/TheReason/master/Database/movies2.json')
+    return fetch('https://raw.githubusercontent.com/BlessedChild/TheReason/master/Database/movies3.json')
       .then((response) => response.json())
       .then((responseJson) => {
 
