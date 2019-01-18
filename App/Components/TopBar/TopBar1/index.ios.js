e5456a4482ae7625b8a6493af38a49f01fd9a4f9
Null <null@NulldeiMac.local>
2019-01-18 09:47:11 +0800

    ++

--- App/Components/TopBar/TopBar1/index.ios.js
+++ App/Components/TopBar/TopBar1/index.ios.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default class HelloWorldApp extends Component {
   render() {
@@ -10,3 +10,23 @@ export default class HelloWorldApp extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      justifyContent: 'center',
+      paddingHorizontal: 10
+  },
+  button: {
+      alignItems: 'center',
+      backgroundColor: '#DDDDDD',
+      padding: 10
+  },
+  countContainer: {
+      alignItems: 'center',
+      padding: 10
+  },
+  countText: {
+      color: '#FF00FF'
+  }
+})
